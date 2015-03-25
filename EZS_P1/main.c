f7704763b2c370c546092c365fe08a796ec99ff3
Markus Klemm <markus@markusklemm.net>
2015-03-25 09:35:11 +0100

    Initial Commit

---
+++ EZS_P1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EZS_P1
+//
+//  Created by Markus Klemm on 25.03.15.
+//  Copyright (c) 2015 net.markusklemm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
